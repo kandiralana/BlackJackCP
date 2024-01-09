8ec53858b94861f3b93541aed87e9ee66817caab
Anastasiia Kandiral <kandiralana@gmail.com>
2024-01-09 17:40:46 +0100

    add possibility to play in the same room

--- game_launch.py
+++ game_launch.py
@@ -15,7 +15,3 @@ if __name__ == '__main__':
     """
     current_game = Game()
     current_game.start_game()
-
-# TODO:  додати можливість залишатись в кімнаті чи іти в іншу
-# треба метод очищення карт перенести в Player Abstract і в кожен клас
-
